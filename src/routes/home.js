980376d563af2d66de48e6ca7f0f04325835742d
JWLD <jackwldavies@icloud.com>
2017-04-05 15:46:59 +0300

    correct indentation and removed console.log

--- src/routes/home.js
+++ src/routes/home.js
@@ -3,13 +3,12 @@ const connPool = require('../../database/db_connection.js');
 
 const handler = (request, reply) => {
   dbQueries.retrieveTransactions(connPool, (err, response) => {
-    console.log(response.rows);
-  let data  = {
-    title: 'Kitty I/O',
-    rows: response.rows
-  }
-  reply.view('home', data)
-})
+    let data  = {
+      title: 'Kitty I/O',
+      rows: response.rows
+    }
+    reply.view('home', data)
+  });
 }
 
 module.exports = {
