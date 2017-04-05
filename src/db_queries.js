50045409693d6ad76718709cc27a0452b1d613d5
JWLD <jackwldavies@icloud.com>
2017-04-05 12:55:24 +0300

    alters default layout

--- src/db_queries.js
+++ src/db_queries.js
@@ -3,7 +3,7 @@ dbQueries = {};
 dbQueries.postTransaction = function(connPool, data, callback) {
   connPool.query(
     'INSERT INTO kitty (name, transaction_value) VALUES ($1, $2)',
-    [data.name, data.transaction_value],
+    [data.name, data.amount],
     callback
   );
 }
