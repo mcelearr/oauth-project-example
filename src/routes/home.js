0350185923de66ff3429d8799bf9efbb6632b3e1
hiba <hibaakel@github.com>
2017-04-05 15:15:59 +0300

    renders home route

--- src/routes/home.js
+++ src/routes/home.js
@@ -1,8 +1,15 @@
+const dbQueries = require('../db_queries.js');
+const connPool = require('../../database/db_connection.js');
+
 const handler = (request, reply) => {
+  dbQueries.retrieveTransactions(connPool, (err, response) => {
+    console.log(response.rows);
   let data  = {
-    title: 'Kitty I/O'
+    title: 'Kitty I/O',
+    rows: response.rows
   }
   reply.view('home', data)
+})
 }
 
 module.exports = {
