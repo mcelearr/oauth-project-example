1cdf2c0e0ffa00ad457cfc913e1dd49d3fe0dcc6
JWLD <jackwldavies@icloud.com>
2017-04-05 18:20:27 +0300

    get total kitty value in the home.js route

--- src/routes/home.js
+++ src/routes/home.js
@@ -3,8 +3,14 @@ const connPool = require('../../database/db_connection.js');
 
 const handler = (request, reply) => {
   dbQueries.retrieveTransactions(connPool, (err, response) => {
+    let total = 0;
+    response.rows.forEach(function(row) {
+      total += row.transaction_value;
+    });
+
     let data  = {
       title: 'Kitty I/O',
+      total: total,
       rows: response.rows
     }
     reply.view('home', data)
