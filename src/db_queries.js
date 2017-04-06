dc088de5591bce156d62c12ce0f6f83ccd21fb57
Mario <mario.saliba98@gmail.com>
2017-04-06 11:56:38 +0300

    adds validitian using joi to th project

--- src/db_queries.js
+++ src/db_queries.js
@@ -1,11 +1,22 @@
+const joi = require('joi');
+
 dbQueries = {};
 
+const schema = joi.object({
+  name: joi.string().regex(/^[a-zA-Z]{3,40}$/),
+  amount: joi.number().integer()
+})
+
 dbQueries.postTransaction = function(connPool, data, callback) {
-  connPool.query(
-    'INSERT INTO kitty (name, transaction_value) VALUES ($1, $2)',
-    [data.name, data.amount],
-    callback
-  );
+  joi.validate({name: data.name, amount: data.amount}, schema, (err, validated) => {
+    console.log(validated);
+    if (err) throw err;
+    connPool.query(
+      'INSERT INTO kitty (name, transaction_value) VALUES ($1, $2)',
+      [validated.name, validated.amount],
+      callback
+    );
+  })
 }
 
 dbQueries.retrieveTransactions = function(connPool, callback) {
