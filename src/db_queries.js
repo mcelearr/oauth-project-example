893cbe07f47a809451a46a5e66386f203230e918
Mario <mario.saliba98@gmail.com>
2017-04-06 12:18:26 +0300

    fix some issues in the pull request

--- src/db_queries.js
+++ src/db_queries.js
@@ -2,15 +2,18 @@ const joi = require('joi');
 
 dbQueries = {};
 
-const schema = joi.object({
-  name: joi.string().regex(/^[a-zA-Z]{3,40}$/),
-  amount: joi.number().integer()
-})
-
 dbQueries.postTransaction = function(connPool, data, callback) {
+  const schema = joi.object({
+    name: joi.string().regex(/^[a-zA-Z]{3,40}$/),
+    amount: joi.number().integer()
+  })
+
   joi.validate({name: data.name, amount: data.amount}, schema, (err, validated) => {
-    console.log(validated);
-    if (err) throw err;
+    if (err) {
+      callback('Invalid Data');
+      return;
+    }
+
     connPool.query(
       'INSERT INTO kitty (name, transaction_value) VALUES ($1, $2)',
       [validated.name, validated.amount],
